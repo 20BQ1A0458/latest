ee72932bfdd5ae9a48308c62e5166316e0ee44bb
Bhargav Ram <163117308+20BQ1A0458@users.noreply.github.com>
2024-10-30 12:14:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-<<<<<<< HEAD
-# app.py
-def main():
-    print("Hello from Dockerized Python application!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, Kubernetes!"
 
 if __name__ == "__main__":
-    main()
-=======
-"print('heltttlo docker,from jenkins')" 
->>>>>>> 69703ff901ee051577ec811abc786e8ff7da3c39
+    app.run(host="0.0.0.0", port=5000)
+
